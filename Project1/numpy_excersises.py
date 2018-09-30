119b56f6f2662b1b9ccfb4306dc830adc62b0529
Aleksander Kusmierczyk <aleksander.kusmierczyk@gmail.com>
2018-09-30 20:38:47 +0200

    Meeting through skype with Valentina and scheduling more refactoring - assertions

--- Project1/numpy_excersises.py
+++ Project1/numpy_excersises.py
@@ -1,12 +1,9 @@
 import numpy as np
-import pandas as pd
 from math import sqrt
-
-pd.set_option("max_columns",50)
 
 
 #Assignment 1 Numpy ex1
-def inner_product(x,y):
+def inner_product(x, y):
     if type(x) == np.ndarray and type(y) == np.ndarray and len(x) == len(y):
         return (x*y).sum()
     else:
@@ -20,39 +17,40 @@ def mean_absolute_error(x, y):
         return (abs(x - y).sum()) / len(x)
     else:
         print("The type of x or y is not np.array or their length is different")
+        return None
 
 
 #Assignment 1 Numpy ex3
-def Lead(x, n):
+def lead(x, n):
     if type(x) == np.ndarray and type(n) == int and len(x) > n:
-        return (np.append(x[n:], ["NaN"] * n))
+        return np.append(x[n:], ["NaN"] * n)
     else:
         print("The type of x is not np.array or the type of n is not int or n is bigger than the x's length")
         return None
 
 
-def Lag(x, n):
+def lag(x, n):
     if type(x) == np.ndarray and type(n) == int and len(x) > n:
-        vector = ["Nan"]* n
-        return (np.append(vector,x[:(len(x)-n)]))
+        vector = ["Nan"] * n
+        return np.append(vector, x[:(len(x)-n)])
     else:
         print("The type of x is not np.array or the type of n is not int")
         return None
 
 
 #Assignment 1 Numpy ex4
-def point_pairwise_distance(x,y):
-    if (type(x) == np.ndarray and type(y) == np.ndarray):
+def point_pairwise_distance(x, y):
+    if type(x) == np.ndarray and type(y) == np.ndarray:
         return sqrt(((x-y)**2).sum())
 
 
-def pairwise_distance(X,y):
-    if type(X) == np.ndarray and type(y) == np.ndarray:
-        if X.shape[1] == y.shape[1]:
-            d = np.zeros(shape=(1, X.shape[0]))
-            for i in range(X.shape[0]):
-                dist = point_pairwise_distance(X[i],y)
-                d[0,i] = dist
+def pairwise_distance(x, y):
+    if type(x) == np.ndarray and type(y) == np.ndarray:
+        if x.shape[1] == y.shape[1]:
+            d = np.zeros(shape=(1, x.shape[0]))
+            for i in range(x.shape[0]):
+                dist = point_pairwise_distance(x[i], y)
+                d[0, i] = dist
             return d
             #np.apply_along_axis(point_pairwise_distance(,y),) ?? can we apply this function ??
         else:
--- Project1/numpy_excersises_tests.py
+++ Project1/numpy_excersises_tests.py
@@ -1,31 +1,31 @@
 import numpy as np
-from numpy_excersises import inner_product,mean_absolute_error,Lead,Lag,point_pairwise_distance,pairwise_distance
+from numpy_excersises import inner_product, mean_absolute_error, lead, lag, point_pairwise_distance, pairwise_distance
 
 #Assignment 1 Numpy ex1 tests:
 def inner_product_test():
     ex1 = inner_product(np.r_[1, 2], np.r_[3, 4])
-    print(ex1)
+    assert(ex1 == 11)
     # wrong length of vectors
     ex1 = inner_product(np.r_[1], np.r_[3, 4])
-    print(ex1)
+    assert(ex1 is None)
     # wrong data type
     ex1 = inner_product("A string", np.r_[3, 4])
     print(ex1)
     # column Wrong dimentions of the matrix-> doesnt have to be covered
     ex1 = inner_product(np.c_[1, 2], np.r_[3, 4])
     print(ex1)
-    A1 = np.r_[1, 2, 3, 4]
-    A2 = np.r_[1, 2, 3, 4]
-    ex1 = inner_product(A1, A2)
+    a1 = np.r_[1, 2, 3, 4]
+    a2= np.r_[1, 2, 3, 4]
+    ex1 = inner_product(a1, a2)
     print("our result")
     print(ex1)
     print("numpy result")
-    print(np.inner(A1, A2))
-    ex1 = inner_product(A1.reshape(-1,1), A2.reshape(-1,1))
+    print(np.inner(a1, a2))
+    ex1 = inner_product(a1.reshape(-1, 1), a2.reshape(-1, 1))
     print("our result")
     print(ex1)
     print("numpy result")
-    print(np.inner(A1, A2))
+    print(np.inner(a1, a2))
     return  None
 
 #Assignment 1 Numpy ex2 tests:
@@ -56,44 +56,43 @@ def mean_absolute_error_test():
 def lead_and_lag_test():
     vector1 = np.r_[1, 2, 3, 4, 5]
     #deleting first three observations
-    ex3 = Lead(vector1, 3)
+    ex3 = lead(vector1, 3)
     print(ex3)
     # deleting first 0 observations
-    ex3 = Lead(vector1, 0)
+    ex3 = lead(vector1, 0)
     print(ex3)
     #checking if Lead works with wrong data types
-    ex3 = Lead("string", 3)
+    ex3 = lead("string", 3)
     print(ex3)
     # checking if Lead works with wrong data types2
-    ex3 = Lead(vector1, "string")
+    ex3 = lead(vector1, "string")
     print(ex3)
     # checking if Lead works with n > x.length
-    ex3 = Lead(vector1, 7)
+    ex3 = lead(vector1, 7)
     print(ex3)
-
     # deleting last 3 observations
-    ex3 = Lag(vector1, 3)
+    ex3 = lag(vector1, 3)
     print(ex3)
     # deleting last 0 observations
-    ex3 = Lag(vector1, 0)
+    ex3 = lag(vector1, 0)
     print(ex3)
     # checking if Lag works with wrong data types
-    ex3 = Lag("string", 3)
+    ex3 = lag("string", 3)
     print(ex3)
     # checking if Lag works with wrong data types2
-    ex3 = Lag(vector1, "string")
+    ex3 = lag(vector1, "string")
     print(ex3)
     # checking if Lag works with n > x.length
-    ex3 = Lag(vector1, 7)
+    ex3 = lag(vector1, 7)
     print(ex3)
 
 
 #Assignment 1 Numpy ex4 tests:
 def pairwise_distance_test():
-    X = np.array([
-    [ 1,  4,  0,  0],
-    [ 1,  0,  0,  2],
-    [ 0, 0, 1, 0]
+    x = np.array([
+    [1,  4,  0,  0],
+    [1,  0,  0,  2],
+    [0, 0, 1, 0]
     ])
     y = np.array([
     [0, 0, 0, 0]
@@ -108,13 +107,13 @@ def pairwise_distance_test():
     ex4 = point_pairwise_distance(y, z)
     print(ex4)
     # calculating the pairwise distance between multiple points and point y
-    ex4 = pairwise_distance(X,y)
+    ex4 = pairwise_distance(x, y)
     print(ex4)
     # testing if the function works for X and y dimentions not matching
-    ex4 = pairwise_distance(X,w)
+    ex4 = pairwise_distance(x, w)
     print(ex4)
     # testing if the function works with wrong data type
-    ex4 = pairwise_distance(X, "a string")
+    ex4 = pairwise_distance(x, "a string")
     print(ex4)
     # testing if the function works with wrong data type
     ex4 = pairwise_distance("a string", w)
