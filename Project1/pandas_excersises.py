105a0f42eadbe9d54006b075eac8655e3eb01885
elcos1 <michalriha2@gmail.com>
2018-10-01 19:49:13 +0200

    Further improvement of the code

--- Project1/pandas_excersises.py
+++ Project1/pandas_excersises.py
@@ -53,6 +53,33 @@ def pandas_excersise1():
     #getting the warmest days
     bestDays = weatherdataJFK.sort_values(by=["temp"], ascending=False).head(5)
 
+def betweenYearAndDay(dataset):
+    list_of_columns = dataset.columns.tolist()
+    
+    if list_of_columns.index("day") > list_of_columns.index("year"):
+        dataset_chosen = dataset.loc[:, "year":"day"]
+    else:
+        dataset_chosen = dataset.loc[:, "day":"year"]
+        
+    print(dataset_chosen)
+
+def outsideYearAndDay(dataset):
+    list_of_columns = dataset.columns.tolist()
+    
+    if list_of_columns.index("day") > list_of_columns.index("year"):
+        dataset_left = dataset.loc[:,:"year"]
+        dataset_right = dataset.loc[:,"day":]
+        dataset_outside_y_and_d = pd.concat([dataset_left, dataset_right], axis=1)
+        
+        print(dataset_outside_y_and_d)
+        
+    else:
+        dataset_left = dataset.loc[:,:"day"]
+        dataset_right = dataset.loc[:,"year":]
+        dataset_outside_y_and_d = pd.concat([dataset_left, dataset_right], axis=1)
+        
+        print(dataset_outside_y_and_d)
+
 def pandas_excersise2():
     #we specify the path, headers and number of lines to be ommited in the file
     dataPath = r'C:\Users\Michal\Documents\GitHub\StatisticsProjectGroupH\Project1\data\nycflights13_flights.csv'
@@ -65,27 +92,10 @@ def pandas_excersise2():
     flight_data = get_dataframe(dataPath, header, skipR)
     
     #We are selecting only columns between Year and Day columns
-    list_of_columns = flight_data.columns.tolist()
-    
-    if list_of_columns.index("day") > list_of_columns.index("year"):
-        flight_data_chosen = flight_data.loc[:, "year":"day"]
-    else:
-        flight_data_chosen = flight_data.loc[:, "day":"year"]
+    betweenYearAndDay(flight_data)
         
     #We are selecting only columns that do not lay between Year and Day columns
-    if list_of_columns.index("day") > list_of_columns.index("year"):
-        flight_data_left = flight_data.loc[:,:"year"]
-        flight_data_right = flight_data.loc[:,"day":]
-        flight_data_between_y_and_d = pd.concat([flight_data_left, flight_data_right], axis=1)
-        
-        print(flight_data_between_y_and_d)
-        
-    else:
-        flight_data_left = flight_data.loc[:,:"day"]
-        flight_data_right = flight_data.loc[:,"year":]
-        flight_data_outside_y_and_d = pd.concat([flight_data_left, flight_data_right], axis=1)
-        
-        print(flight_data_outside_y_and_d)
+    outsideYearAndDay(flight_data)
     
 def pandas_excersise3():
 
