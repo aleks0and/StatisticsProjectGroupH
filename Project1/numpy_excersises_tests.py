97b44715923d2d7d9778fe6094ae5f2ac9f7f608
ValentinaParietti <parietti.valentina@gmail.com>
2018-10-02 14:59:07 +0200

    rewritten tests for ex 2-3-4 np
    Changed print with assert in the tests for ex 2-3-4 np. I was not able to use assert correclty for one test in ex 4 np

--- Project1/numpy_excersises_tests.py
+++ Project1/numpy_excersises_tests.py
@@ -28,57 +28,57 @@ def mean_absolute_error_test():
     vector2 = np.r_[1, 2, 5, 8, 7]
     # two rows
     ex2 = mean_absolute_error(vector1, vector2)
-    print(ex2)
+    assert(ex2 == 0)
     # two different vectors
     vector3 = np.r_[1, 2, 3, 4, 7]
     ex2 = mean_absolute_error(vector1, vector3)
-    print(ex2)
+    assert(ex2 == 1.2)
     #two columns
     ex2 = mean_absolute_error(vector1.reshape(-1, 1), vector2.reshape(-1, 1))
-    print (ex2)
+    assert(ex2 == 0)
     ex2 = mean_absolute_error(vector1.reshape(-1, 1), vector3.reshape(-1, 1))
-    print(ex2)
+    assert(ex2 == 1.2)
     #wrong data type
     ex2 = mean_absolute_error("a string", vector2.reshape(-1, 1))
-    print(ex2)
+    assert(ex2 is None)
     # wrong length of one vector
     vector4 = np.r_[1,2,3]
     ex2 = mean_absolute_error(vector1, vector4)
-    print(ex2)
+    assert(ex2 is None)
 
 #Assignment 1 Numpy ex3 tests:
 def lead_and_lag_test():
     vector1 = np.r_[1, 2, 3, 4, 5]
     #deleting first three observations
     ex3 = lead(vector1, 3)
-    print(ex3)
+    np.testing.assert_array_equal(ex3, [4, 5, "NaN", "NaN", "NaN"])
     # deleting first 0 observations
     ex3 = lead(vector1, 0)
-    print(ex3)
+    np.testing.assert_array_equal(ex3, [1., 2., 3., 4., 5.])
     #checking if Lead works with wrong data types
     ex3 = lead("string", 3)
-    print(ex3)
+    assert(ex3 is None)
     # checking if Lead works with wrong data types2
     ex3 = lead(vector1, "string")
-    print(ex3)
+    assert(ex3 is None)
     # checking if Lead works with n > x.length
     ex3 = lead(vector1, 7)
-    print(ex3)
+    assert(ex3 is None)
     # deleting last 3 observations
     ex3 = lag(vector1, 3)
-    print(ex3)
+    np.testing.assert_array_equal(ex3, ["NaN", "NaN", "NaN", 1, 2])
     # deleting last 0 observations
     ex3 = lag(vector1, 0)
-    print(ex3)
+    np.testing.assert_array_equal(ex3, [1., 2., 3., 4., 5.])
     # checking if Lag works with wrong data types
     ex3 = lag("string", 3)
-    print(ex3)
+    assert(ex3 is None)
     # checking if Lag works with wrong data types2
     ex3 = lag(vector1, "string")
-    print(ex3)
+    assert(ex3 is None)
     # checking if Lag works with n > x.length
     ex3 = lag(vector1, 7)
-    print(ex3)
+    assert(ex3 is None)
 
 
 #Assignment 1 Numpy ex4 tests:
@@ -98,19 +98,19 @@ def pairwise_distance_test():
     [0, 1, 2]
     ])
     # calculating a pairwise distance between two points
-    ex4 = point_pairwise_distance(y, z)
-    print(ex4)
+    ex4 = point_pairwise_distance(y, z) 
+    assert(ex4 == 4.123105625617661)
     # calculating the pairwise distance between multiple points and point y
     ex4 = pairwise_distance(x, y)
-    print(ex4)
+    print(ex4) #I cannot make assert work in this case
     # testing if the function works for X and y dimentions not matching
     ex4 = pairwise_distance(x, w)
-    print(ex4)
+    assert(ex4 is None)
     # testing if the function works with wrong data type
     ex4 = pairwise_distance(x, "a string")
-    print(ex4)
+    assert(ex4 is None)
     # testing if the function works with wrong data type
     ex4 = pairwise_distance("a string", w)
-    print(ex4)
+    assert(ex4 is None)
 
     return None
