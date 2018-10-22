70616fc5d1391bc45cc4b338916803cd282e3639
Enrica Archetti <enricaarchetti@gmail.com>
2018-10-23 00:31:03 +0200

    first implementation of main

--- Project2/Main.py
+++ Project2/Main.py
@@ -1,7 +1,48 @@
 #implementation of 2nd project for Statistics
-from hierarchical_cluster_exercise import classes_exercise
+from hierarchical_cluster_exercise import hierarchical_cluster_analysis, assignment2_point2_specified
+from PCA_exercise import pca_exercise
+from PCA_components_comparison_exercise import principal_components_comparison_given_data, principal_components_comparison_given_data_all_in_one, principal_components_comparison_3by3
+from Kmeans_extraction_exercise import best_k_for_kmeans, best_k_for_kmeans_given_data
+from Kmeans_exercise import assignment2_point3, assignment2_point3_top2_eigenvalues, silhouette, original_vars_PCA
 
 def main():
 
-   # classes_exercise()
+    #point 1: PCA
+    pca_exercise()
+    
+    #point 2: hierarchical cluster analysis
+    hierarchical_cluster_analysis()
+    assignment2_point2_specified()
+    
+    #point 3: k-means cluster analysis
+    assignment2_point3()
+    assignment2_point3_top2_eigenvalues()
+    
+    #point 3.1: sillhouette
+    silhouette()
+    
+    #point 3.2: Plot on the space of the first two dimensions of the PCA the clusters obtained with K-means
+    best_k_for_kmeans()
+    best_k_for_kmeans_given_data
+    
+    principal_components_comparison_given_data()
+    principal_components_comparison_given_data_all_in_one()
+    
+    #point 3.3: 
+    original_vars_PCA()
+    
+    
+    #point 4:
+    
+    #point 5:
+    principal_components_comparison_given_data()
+    principal_components_comparison_given_data_all_in_one()
+    principal_components_comparison_3by3()
+    
+    
+    
+   
+    
+    
+    
     return None
