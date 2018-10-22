5407d40b5d6c7b7392adc8fe9328fb8e53329379
Aleksander Kusmierczyk <aleksander.kusmierczyk@gmail.com>
2018-10-22 13:08:41 +0200

    added function to plot all the PCs on one plot

--- Project2/PCA_components_comparison_exercise.py
+++ Project2/PCA_components_comparison_exercise.py
@@ -25,12 +25,13 @@ def principal_components_comparison_given_columns(column_id):
     compared_vectors_name = []
     plt.figure(figsize=(15,9))
     plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    skipped = 0
     for i in range(0,len(PCs)):
         if i != column_id:
             compared_vectors_name.clear()
             compared_vectors_name.append(feature_names[column_id])
             compared_vectors_name.append(feature_names[i])
-            plt.subplot(X_axis, Y_axis, i+1)
+            plt.subplot(X_axis, Y_axis, i+1 - skipped)
             plt.axis('equal')
             plt.xlim([-1.0, 1.0])
             plt.ylim([-1.0, 1.0])
@@ -48,6 +49,8 @@ def principal_components_comparison_given_columns(column_id):
                 plt.text(j, i, z, ha='center', va='center')
             circle = plt.Circle((0, 0), 0.5, facecolor='none', edgecolor='b')
             plt.gca().add_artist(circle)
+        else:
+            skipped = 1
 
     plt.show()
 
@@ -100,12 +103,45 @@ def principal_components_comparison_given_data(data, column_id):
     plt.show()
 
 
+def principal_components_comparison_given_data_all_in_one(data):
+    x_s = quantify_data(data, True)
+    covariance_matrix = np.cov(x_s.T)
+    eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
+    eigen_vectors_values = [(np.abs(eigen_values[i]), eigen_vectors[:, i])
+                            for i in range(len(eigen_vectors))]
+    eigen_vectors_values.sort()
+    eigen_vectors_values.reverse()
+    top2_eigenvectors = np.hstack((eigen_vectors_values[0][1].reshape(len(eigen_values), 1),
+                                   eigen_vectors_values[1][1].reshape(len(eigen_values), 1),
+                                   ))
+    py.offline.init_notebook_mode(connected=True)
+    PCs = eigen_vectors
+    colors = cm.rainbow(np.linspace(0, 1, len(PCs)))
+    fig = plt.figure(figsize=(5, 5))
+    plt.quiver(np.zeros(PCs.shape[1]), np.zeros(PCs.shape[1]),
+               PCs[0, ], PCs[1, :],
+               angles='xy', scale_units='xy', scale=1, color=colors)
+    feature_names = list(data)
+    for i, j, z in zip(PCs[1, :]+0.02, PCs[0, :]+0.02, feature_names):
+        plt.text(j, i, z, ha='center', va='center')
+    circle = plt.Circle((0, 0), 1, facecolor='none', edgecolor='b')
+    plt.gca().add_artist(circle)
+
+    plt.axis('equal')
+    plt.xlim([-1.0, 1.0])
+    plt.ylim([-1.0, 1.0])
+
+    plt.xlabel('PC 0')
+    plt.ylabel('PC 1')
+
+    plt.show()
 
 # Run the comparison between the 3rd PC and all the others
-principal_components_comparison_given_columns(2)
+# principal_components_comparison_given_columns(2)
 # Run the comparison between the 4th PC and all the others given dataset.
 #principal_components_comparison_given_columns(3)
 # we assume that the dataset is prepraed beferehand.
-#path = r'./data/wines_properties.csv'
-#wine_data = prepare_and_load_data(path, skip_rows=0)
-#principal_components_comparison_given_data(wine_data, 1)
+# path = r'./data/wines_properties.csv'
+# wine_data = prepare_and_load_data(path, skiprows=0)
+# #principal_components_comparison_given_data(wine_data, 1)
+# principal_components_comparison_given_data_all_in_one(wine_data)
